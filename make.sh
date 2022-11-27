848df2de67cc93ca8f7f368063c1991752a822bd
isak <isak.levinson@gmail.com>
2022-11-27 20:08:13 +0200

    cleanups: removed unnesesary files

--- make.sh
+++ make.sh
@@ -5,4 +5,4 @@ astyle --suffix=none --formatted --options=.astyle --recursive *.c,*.cc,*.h
 #g++ -ludev suinput.o -o test main.cc -Wall
 #g++ -Wall -std=c++11 -ludev -lpthread -lX11 -llua5.2 suinput.c joystick.cc LuaScript.cc CVirtualJoy.cc CVirtualKeyboard.cc -o wejoy main.cc
 #g++ test.cc joystick.cc -std=c++0x -Wall -o test
-g++ -Wall -std=c++11 suinput.c joystick.cc CKeyboard.cc LuaScript.cc CVirtualJoy.cc CVirtualKeyboard.cc linuxtrack.c -o wejoy main.cc -ludev -lpthread -lX11 -llua5.2
+g++ -Wall -std=c++11 suinput.c CVirtualJoy.cc linuxtrack.c -o wejoy main.cc -ludev -lpthread -lX11 -llua5.2
